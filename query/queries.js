268983913ec4065b802ee77cc5053fe4448acdfb
Jaime José Romero Barreto <122017219+imjrbarreto@users.noreply.github.com>
2026-06-25 12:16:31 +0200

    Update queries

--- query/queries.js
+++ query/queries.js
@@ -38,22 +38,23 @@ db.restaurants.find({ "address.coord.0": { $lt: -95.754168 } }, { _id: 0 });
 
 
 // 14. Trobar restaurant_id, name, borough i cuisine on el nom comença amb 'Wil'.
-
+db.restaurants.find({ name: /^Wil/ }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 // 15. Trobar restaurant_id, name, borough i cuisine on el nom acaba en 'ces'.
-
+db.restaurants.find({ name: /ces$/ }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 // 16. Trobar restaurant_id, name, borough i cuisine on el nom conté 'Reg'.
+db.restaurants.find({ name: /Reg/ }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 
 // 17. Trobar restaurants del Bronx que preparen cuina americana o xinesa.
-
+db.restaurants.find({ borough: "Bronx", cuisine: { $in: ["American", "Chinese"] } }, { _id: 0 });
 
 // 18. Trobar restaurant_id, name, borough i cuisine per a Staten Island, Queens, Bronx o Brooklyn.
-
+db.restaurants.find({ borough: { $in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 // 19. Trobar restaurant_id, name, borough i cuisine per a restaurants que NO són d'aquests barris.
-
+db.restaurants.find({ borough: { $nin: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } }, { _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
 
 // 20. Trobar restaurant_id, name, borough i cuisine amb marcador no superior a 10.
 
@@ -71,10 +72,10 @@ db.restaurants.find({ "address.coord.0": { $lt: -95.754168 } }, { _id: 0 });
 
 
 // 25. Ordenar els noms dels restaurants en ordre ascendent, mostrant totes les columnes.
-
+db.restaurants.find({}, { _id: 0 }).sort({ name: 1 });
 
 // 26. Ordenar els noms dels restaurants en ordre descendent, mostrant totes les columnes.
-
+db.restaurants.find({}, { _id: 0 }).sort({ name: -1 });
 
 // 27. Ordenar cuisine ascendent i borough descendent.
 
